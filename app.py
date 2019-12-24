ad431e5c1a86103b91e0602586bbe6b10633ca95
victorgbd <53549825+victorgbd@users.noreply.github.com>
2019-12-24 18:00:02 -0400

    Update app.py
    comentarios a la api

--- app.py
+++ app.py
@@ -1,36 +1,15 @@
 from flask import Flask, request
-import json
-from AI import AI
 from predict import RED
 app = Flask(__name__)
+#instanciamos el objeto RED
 redn = RED()
+
 @app.route("/upload/<string:file>")
+#se pasa por parametros la direccion de la imagen
 def upload(file):
     prediccion = redn.predict(file)
-    return prediccion
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        f = request.files["file"]
-        filename = f.filename
-        print(f)
-        print(filename)
-        prediccion = redn.predict(filename)
-        return prediccion
-
-    return """<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Upload File</title>
-</head>
-<body>
-    <h1>Upload File</h1>
-    <form method="POST" enctype="multipart/form-data">
-        <input type="file" name="file">
-        <input type="submit" value="Upload">
-    </form>
-</body>
-</html>"""    
+    #prediccion contiene el json a retornar
+    return prediccion    
 if __name__ == "__main__":
+    #corremos nuetro servidors
     app.run(debug=True,port=5000)
