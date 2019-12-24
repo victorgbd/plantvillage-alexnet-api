5c98a21e22dc39ff76629678b693de56199edefc
victorgbd <53549825+victorgbd@users.noreply.github.com>
2019-12-24 18:54:52 -0400

    Update entrenar.py
    comentarios agregados

--- entrenar.py
+++ entrenar.py
@@ -8,29 +8,46 @@ from tensorflow.python.keras.layers import  Convolution2D, MaxPooling2D
 from tensorflow.python.keras.layers.normalization import BatchNormalization
 from tensorflow.python.keras import backend as K
 from keras import layers
+#limpiamos cualquier sesión que haya abierte en keras
 K.clear_session()
 
 
 
+#La direccion de nuestro dataset de entrenamiento y de validacion
 data_entrenamiento = './data/train'
 data_validacion = './data/valid'
 
 """
-Parameters
+Parametros
 """
 epocas=25
+#tamaño de las imagenes
 longitud, altura = 224, 224
+
+#tamaño de la cantidad de ejemplos que se evaluaran por epoca
 batch_size = 128
+
+#cantidad de filtros para cada capa
 filtrosConv1 = 96
 filtrosConv25 = 256
 filtrosConv34 = 384
+
+#tamaño de la ventana de max pooling 
 tamano_pool = (2, 2)
+
+#el numero de clases que deseamos predecir o nuestros targets
 clases = 3
+#el learning rate o ratio de aprendizaje
 lr = 0.001
 
 
-##Preparamos nuestras imagenes
+#Preparamos nuestras imagenes
 
+"""
+rescalamos las imagenes a 255 pixeles
+aplicamos ciertos movimientos y zooms en las imagenes para lograr que 
+no solo aprenda que nuestro target este en un solo lugar de la imagen
+"""
 entrenamiento_datagen = ImageDataGenerator(
     rescale=1. / 255,
     shear_range=0.2,
@@ -40,58 +57,61 @@ entrenamiento_datagen = ImageDataGenerator(
     fill_mode='nearest')
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
-
+#aqui especificamos donde esta nuestra data, el tamaño de esta, el batch size y como se clasificara nuestra data
 entrenamiento_generador = entrenamiento_datagen.flow_from_directory(
     data_entrenamiento,
     target_size=(altura, longitud),
     batch_size=batch_size,
     class_mode='categorical')
-
+#lo mismo para la data de validación o test
 validacion_generador = test_datagen.flow_from_directory(
     data_validacion,
     target_size=(altura, longitud),
     batch_size=batch_size,
     class_mode='categorical')
 #directorios de las clases
-#clase_direct = entrenamiento_generador.class_indices
-#lista de las clase
-#li = list(class_dict.keys())
-# Initializing the CNN
+#nos servirá para identificar las predicciones que nos da el modelo ya entrenado(utilizar en un notebook como recomendacion)
+"""
+clase_direct = entrenamiento_generador.class_indices
+lista de las clase
+li = list(class_dict.keys())
+"""
+# inicializando la CNN
 classifier = Sequential()
 
-# Convolution Step 1
+# Capa de convolucion Paso 1
 classifier.add(Convolution2D(filtrosConv1, 11, strides = (4, 4), padding = 'valid', input_shape=(224, 224, 3), activation = 'relu',trainable=False))
 
-# Max Pooling Step 1
+# Capa de  Max Pooling  Paso 1
 classifier.add(MaxPooling2D(pool_size = tamano_pool, strides = (2, 2), padding = 'valid', trainable=False))
 classifier.add(BatchNormalization( trainable=False))
 
-# Convolution Step 2
+# Capa de convolucion Paso 2
 classifier.add(Convolution2D(filtrosConv25, 11, strides = (1, 1), padding='valid', activation = 'relu', trainable=False))
 
-# Max Pooling Step 2
+# Capa de  Max Pooling  Paso 2
 classifier.add(MaxPooling2D(pool_size = tamano_pool, strides = (2, 2), padding='valid', trainable=False))
 classifier.add(BatchNormalization(trainable=False))
 
-# Convolution Step 3
+# Capa de convolucion Paso 3
 classifier.add(Convolution2D(filtrosConv34, 3, strides = (1, 1), padding='valid', activation = 'relu', trainable=False))
 classifier.add(BatchNormalization(trainable=False))
 
-# Convolution Step 4
+# Capa de convolucion Paso 4
 classifier.add(Convolution2D(filtrosConv34, 3, strides = (1, 1), padding='valid', activation = 'relu', trainable=False))
 classifier.add(BatchNormalization(trainable=False))
 
-# Convolution Step 5
+# Capa de convolucion Paso 5
 classifier.add(Convolution2D(filtrosConv25, 3, strides=(1, 1), padding='valid', activation = 'relu', trainable=False))
 
-# Max Pooling Step 3
+# Capa de  Max Pooling  Paso 3
 classifier.add(MaxPooling2D(pool_size = tamano_pool, strides = (2, 2), padding = 'valid', trainable=False))
 classifier.add(BatchNormalization(trainable=False))
 
-# Flattening Step
+# Paso de aplastamiento o flattening
 classifier.add(Flatten(trainable=False))
 
-# Full Connection Step
+# Paso de Full Connection 
 classifier.add(Dense(units = 4096, activation = 'relu', trainable=False))
 classifier.add(Dropout(0.4, trainable=False))
 classifier.add(BatchNormalization(trainable=False))
@@ -103,24 +123,40 @@ classifier.add(Dropout(0.2))
 classifier.add(BatchNormalization())
 classifier.add(Dense(units = clases, activation = 'softmax'))
 
+#se muestran cada una de las capas y la cantidad de parametros que pasaran cada una de estas capas
 for layer in enumerate(classifier.layers):
     print(layer)
 print (classifier.summary())
+
+"""
+se compila la nuestro modelo especificando nuestra funcion de coste, 
+el optimizador y sus hiperparametros y la metrica como se evaluará nuestro coste
+"""
 classifier.compile(loss='categorical_crossentropy',
             optimizer=optimizers.SGD(lr=lr,momentum=0.9, decay=0.005),
             metrics=['accuracy'])
 
+#esta es la cantidad de ejemplos que tenemos en nuestra data de entrenamiento y validación
 train_num = entrenamiento_generador.samples
 valid_num = validacion_generador.samples
-
 print(train_num,valid_num)
+
+#entrenamos el modelos
 classifier.fit_generator(
+    #data de entrenamiento
     entrenamiento_generador,
+    #los pasos que dara por cada epoca
+    #es la division floor de el numero de los ejemplos y el batch_size especificado
     steps_per_epoch=train_num//batch_size,
+    #el numero de epocas o iteraciones
     epochs=epocas,
+    #data de validacion o test
     validation_data=validacion_generador,
+    #los pasos que dara por cada epoca
+    #es la division floor de el numero de los ejemplos y el batch_size especificado
     validation_steps=valid_num//batch_size)
 
+#directorio donde se guardara el modelo y los pesos
 target_dir = './modelo/'
 if not os.path.exists(target_dir):
   os.mkdir(target_dir)
