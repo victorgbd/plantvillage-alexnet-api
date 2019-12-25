2c8f35e6658187908a2383d26d77817b901047d7
victorgbd <53549825+victorgbd@users.noreply.github.com>
2019-12-25 17:34:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,32 @@ redn = RED()
 def upload(file):
     prediccion = redn.predict(file)
     #prediccion contiene el json a retornar
-    return prediccion    
+    return prediccion
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        f = request.files["file"]
+        filename = f.filename
+        print(f)
+        print(filename)
+        prediccion = redn.predict(filename)
+        return prediccion
+
+    return """<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Upload File</title>
+</head>
+<body>
+    <h1>Upload File</h1>
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit" value="Upload">
+    </form>
+</body>
+</html>"""
 if __name__ == "__main__":
     #corremos nuetro servidor
     app.run(debug=True,port=5000)
